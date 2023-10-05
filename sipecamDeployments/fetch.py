0c87c28f9bdf347c0800cfcc9c56620832e87778
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2023-10-05 15:26:59 -0600

    Change the datatype for the output printed, from dict to json.

--- sipecamDeployments/fetch.py
+++ sipecamDeployments/fetch.py
@@ -36,7 +36,7 @@ def kobo_deployments():
 
     filtered = get_surveys(common_name, headers)
 
-    # before making another request 
+    # before making another request
     # wait 2 second to not overload the server
     time.sleep(2)
     for r in get_data(filtered, headers):
@@ -52,7 +52,7 @@ def kobo_deployments():
                     clean_data[n]["coordinates_warning"] = f"Coordinates corrected by hand from (lat, lon) = ({latitude},{longitude})"
                 else:
                     clean_data[n]["coordinates_warning"] = None
-        
+
         cumulus_data = get_cumulus_nodes(
                 session,
                 int(cumulus_name),
@@ -80,15 +80,19 @@ def current_kobo_deployments():
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Get kobo deployments and match zendro information from SiPeCaM surveys. The following environtmental variables should be declared in order to get the result: "KOBO_URL", "KOBO_USER", "KOBO_PASSWORD", "ZENDRO_URL", "ZENDRO_USER", "ZENDRO_PASSWORD"')
+    parser = argparse.ArgumentParser(description='''Get kobo deployments
+    and match zendro information from SiPeCaM surveys. The following
+    environtmental variables should be declared in order to get the
+    result: "KOBO_URL", "KOBO_USER", "KOBO_PASSWORD", "ZENDRO_URL",
+    "ZENDRO_USER", "ZENDRO_PASSWORD"''')
     parser.add_argument('--output', default=None, required=False,
                         help='A valid output path. If not specified, output will be printed to stdout.')
     args = parser.parse_args()
-    
+
     depls = current_kobo_deployments()
-    
+
     if args.output is not None:
         with open(args.output, "w") as f:
             json.dump(depls, f)
     else:
-        print(depls)
+        print(json.dumps(depls, indent=2))
