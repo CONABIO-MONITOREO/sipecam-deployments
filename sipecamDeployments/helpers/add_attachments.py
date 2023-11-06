fa8972755f506b42b496863452d164a668f934e1
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2023-11-06 12:11:47 -0600

    fix: Attachments

--- sipecamDeployments/helpers/add_attachments.py
+++ sipecamDeployments/helpers/add_attachments.py
@@ -15,53 +15,93 @@ def get_metadata_by_question(content, download_url, t):
     question = build_question_metadata(str(t[0]), field_name, download_url)
     return question
 
-def add_attachments(report,content):
-    """
-        Add attachment url to metadata as a string
+# def add_attachments(report,content):
+#     """
+#         Add attachment url to metadata as a string
 
-        Parameters:
-            report (dict):  A dict containing the report data
-                            of the survey for a specific device
-            content (list): A list containing info of the survey.
+#         Parameters:
+#             report (dict):  A dict containing the report data
+#                             of the survey for a specific device
+#             content (list): A list containing info of the survey.
 
-        Returns:
-            metadata (string):  A string containing the attachment urls
-                                in a json structure.
-     """
+#         Returns:
+#             metadata (string):  A string containing the attachment urls
+#                                 in a json structure.
+#      """
 
-    metadata = "{ "
-    for idx, file in enumerate(report["_attachments"]):
-        """
-            Iterate through the attachments to add the file
-            url to the metadata field of deployment
-          """
-        url = file["download_url"]
-        filename = file["filename"].split("/")[4]
-        items = [(index, item) for index, item in enumerate(report.items())]
-        filter_by_filename = list(filter(lambda t: isinstance(t[1][1], str) and t[1][1] == filename, items))
-        get_question = partial(get_metadata_by_question, content, url)
-        questions = list(map(get_question, filter_by_filename))
-        metadata += ",".join(questions)
-        if idx != len(report["_attachments"]) - 1:
-            metadata += ","
+#     metadata = "{ "
+#     for idx, file in enumerate(report["_attachments"]):
+#         """
+#             Iterate through the attachments to add the file
+#             url to the metadata field of deployment
+#           """
+#         url = file["download_url"]
+#         filename = file["filename"].split("/")[4]
+#         items = [(index, item) for index, item in enumerate(report.items())]
+#         filter_by_filename = list(filter(lambda t: isinstance(t[1][1], str) and t[1][1] == filename, items))
+#         get_question = partial(get_metadata_by_question, content, url)
+#         questions = list(map(get_question, filter_by_filename))
+#         metadata += ",".join(questions)
+#         if idx != len(report["_attachments"]) - 1:
+#             metadata += ","
 
-        if len(metadata) < 3:
+#         if len(metadata) < 3:
+#             all_items = []
+#             for index, (key, value) in enumerate(report.items()):
+#                 if isinstance(value,list):
+#                     all_questions = []
+#                     for i in value:
+#                         if isinstance(i,dict):
+#                             questions = []
+#                             for name, item in i.items():
+#                                 if isinstance(item,str) and item == filename:
+#                                     key = name.split("/")[1]
+#                                     field_label = field_label_by_key(key, content)
+#                                     field_name = get_field_name(key, field_label)
+#                                     questions.append(build_question_metadata(str(index), field_name, url))
+#                             all_questions.append(",".join(questions))
+#                     all_items.append(",".join(all_questions))
+#             metadata += ",".join(all_items)
+#     metadata += "}"
+
+#     return metadata
+
+
+def filter_by_filename(filename, items):
+    return list(filter(lambda t: isinstance(t[1][1], str) and t[1][1] == filename, items))
+
+def add_attachments(report, content):
+    attachments = [(i, f) for i, f in enumerate(report['_attachments'])]
+    items = [(j, it) for j, it in enumerate(report.items())]
+    questions = []
+    for idx, _file in attachments:
+        url = _file['download_url']
+        filename = _file['filename'].split('/')[4]
+        filtered = filter_by_filename(filename, items)
+        get_question = partial(get_metada_by_question, content, url)
+        qs = list(map(get_question, filtered))
+        if qs:
+            questions.extend(qs)
+
+        if not qs:
             all_items = []
-            for index, (key, value) in enumerate(report.items()):
-                if isinstance(value,list):
+            for index, (key, value) in items:
+                if isinstance(value, list):
                     all_questions = []
                     for i in value:
-                        if isinstance(i,dict):
-                            questions = []
+                        if isinstance(i, dict):
+                            qs = []
                             for name, item in i.items():
-                                if isinstance(item,str) and item == filename:
-                                    key = name.split("/")[1]
+                                if isinstance(item, str) and item == filename:
+                                    key = name.split('/')[1]
                                     field_label = field_label_by_key(key, content)
                                     field_name = get_field_name(key, field_label)
-                                    questions.append(build_question_metadata(str(index), field_name, url))
-                            all_questions.append(",".join(questions))
-                    all_items.append(",".join(all_questions))
-            metadata += ",".join(all_items)
-    metadata += "}"
-
-    return metadata
+                                    qs.append(build_question_metadata(str(index), field_name, url))
+                            if qs:
+                                all_questions.extend(qs)
+                    if all_questions:
+                        all_items.extend(all_questions)
+            if all_items:
+                questions.extend(all_items)
+        metadata = '{' + ','.join(all_items) + '}'
+        return metadata
